94644f205d15acfc0eb6d72619b43ebc66c9128a
chaincpl-2542 <chaincpl2542@gmail.com>
2025-03-18 16:41:35 +0700

    update cryptex

--- Scripts/CryptexController.cs
+++ Scripts/CryptexController.cs
@@ -1,99 +1,90 @@
-using Godot.Collections;
 using Godot;
+using Godot.Collections;
 
 public partial class CryptexController : Node
 {
-	private const string PASS_CODE = "911";
-	private int[] _codeNumbers = new int[3];
-	
-	[Export] public Array<PackedScene> NumberList = new Array<PackedScene>();
+    private const string PASS_CODE = "911";
+    private int[] _codeNumbers = new int[3];
 
-	[Export] public Node firstNumberNew;
-	[Export] public Node firstNumberOld;
-	[Export] public Node firstNumberNewPosition;
-	[Export] public Node firstNumberOldPosition;
+    [Export] public Array<PackedScene> NumberList = new Array<PackedScene>();
+    private Node3D firstNumber;
+    private Node3D secondNumber;
+    private Node3D thirdNumber;
 
-	public override void _Ready()
-	{
-		firstNumberNew = NumberList[0].Instantiate();
-		firstNumberOld = firstNumberNew;
-		
-		AddChild(firstNumberNew);
-		
-		firstNumberNew = null;
-	}
+    [Export] public Node3D firstNumberPosition;
+    [Export] public Node3D secondNumberPosition;
+    [Export] public Node3D thirdNumberPosition;
 
-	public void SpawnNumber(int number)
-	{
-		firstNumberNew = NumberList[number - 1].Instantiate();
-		
-		firstNumberOld.QueueFree();
-		firstNumberOld = null;
-		
-		firstNumberOld = firstNumberNew;
-		AddChild(firstNumberNew);
+    public override void _Ready()
+    {
+        firstNumber = InstantiateNumber(0, firstNumberPosition);
+        secondNumber = InstantiateNumber(0, secondNumberPosition);
+        thirdNumber = InstantiateNumber(0, thirdNumberPosition);
+    }
 
-		firstNumberNew = null;
-	}
-	
-	public void OnIncreaseButtonPressed(int position)
-	{
-		ChangeNumber(position, 1);
-	}
+    public void ChangeNumber(int position, int change)
+    {
+        if (position >= 0 && position < _codeNumbers.Length)
+        {
+            _codeNumbers[position] += change;
 
-	public void OnDecreaseButtonPressed(int position)
-	{
-		ChangeNumber(position, -1);
-	}
-	
-	private void ChangeNumber(int position, int change)
-	{
-		if (position >= 0 && position < _codeNumbers.Length)
-		{
-			_codeNumbers[position] += change;
+            if (_codeNumbers[position] > 9) _codeNumbers[position] = 0;
+            if (_codeNumbers[position] < 0) _codeNumbers[position] = 9;
 
-			if (_codeNumbers[position] > 9) _codeNumbers[position] = 0;
-			if (_codeNumbers[position] < 0) _codeNumbers[position] = 9;
-		}
+            ReplaceNumber(position, _codeNumbers[position]);
 
-		GD.Print($"Updated Code: {_codeNumbers[0]} {_codeNumbers[1]} {_codeNumbers[2]}");
-		
-		if (CheckCode())
-		{
-			UnlockCryptex();
-		}
-	}
-	
-	private bool CheckCode()
-	{
-		string enteredCode = $"{_codeNumbers[0]}{_codeNumbers[1]}{_codeNumbers[2]}";
-		return enteredCode == PASS_CODE;
-	}
-	
-	private void UnlockCryptex()
-	{
-		GD.Print("Cryptex Unlocked! You got the key!");
-	}
-	
-	public void OnIncreaseButton1Pressed() => ChangeNumber(0, 1);
-	public void OnDecreaseButton1Pressed() => ChangeNumber(0, -1);
-	public void OnIncreaseButton2Pressed() => ChangeNumber(1, 1);
-	public void OnDecreaseButton2Pressed() => ChangeNumber(1, -1);
-	public void OnIncreaseButton3Pressed() => ChangeNumber(2, 1);
-	public void OnDecreaseButton3Pressed() => ChangeNumber(2, -1);
-	
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventKey keyEvent && keyEvent.Pressed && !keyEvent.Echo)
-		{
-			if (keyEvent.Keycode == Key.Key1)
-			{
-				SpawnNumber(4);
-			}
-			else if (keyEvent.Keycode == Key.Key2)
-			{
-				SpawnNumber(9);
-			}
-		}
-	}
+            GD.Print($"Updated Code: {_codeNumbers[0]} {_codeNumbers[1]} {_codeNumbers[2]}");
+
+            if (CheckCode())
+            {
+                UnlockCryptex();
+            }
+        }
+    }
+
+    private Node3D InstantiateNumber(int numberIndex, Node3D position)
+    {
+        Node3D instance = NumberList[numberIndex].Instantiate<Node3D>();
+        AddChild(instance);
+        instance.GlobalTransform = new Transform3D(Basis.Identity, position.Position);
+        return instance;
+    }
+
+    private void ReplaceNumber(int position, int newNumber)
+    {
+        switch (position)
+        {
+            case 0:
+                firstNumber.QueueFree();
+                firstNumber = InstantiateNumber(newNumber, firstNumberPosition);
+                break;
+            case 1:
+                secondNumber.QueueFree();
+                secondNumber = InstantiateNumber(newNumber, secondNumberPosition);
+                break;
+            case 2:
+                thirdNumber.QueueFree();
+                thirdNumber = InstantiateNumber(newNumber, thirdNumberPosition);
+                break;
+        }
+    }
+
+    private bool CheckCode()
+    {
+        string enteredCode = $"{_codeNumbers[0]}{_codeNumbers[1]}{_codeNumbers[2]}";
+        return enteredCode == PASS_CODE;
+    }
+
+    // Called when the code is correct
+    private void UnlockCryptex()
+    {
+        GD.Print("Cryptex Unlocked! You got the key!");
+    }
+
+    public void OnIncreaseButton1Pressed() => ChangeNumber(0, 1);
+    public void OnDecreaseButton1Pressed() => ChangeNumber(0, -1);
+    public void OnIncreaseButton2Pressed() => ChangeNumber(1, 1);
+    public void OnDecreaseButton2Pressed() => ChangeNumber(1, -1);
+    public void OnIncreaseButton3Pressed() => ChangeNumber(2, 1);
+    public void OnDecreaseButton3Pressed() => ChangeNumber(2, -1);
 }
