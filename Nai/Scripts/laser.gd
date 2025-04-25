ccf9bfb9ce3964cabfbfc536051ffd779d6dd17d
Knight Nai <naddanaiparod55@gmail.com>
2025-04-25 20:14:26 +0700

    fix: imp with particle

--- Nai/Scripts/laser.gd
+++ Nai/Scripts/laser.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
 @onready var beam_mesh = $BeamMesh
+@onready var end_particles = $EndParticles
 
 func  _process(delta: float) -> void:
 	var cast_point
@@ -11,3 +12,5 @@ func  _process(delta: float) -> void:
 		
 		beam_mesh.mesh.height = cast_point.y
 		beam_mesh.position.y = cast_point.y/2
+		
+		end_particles.position.y = cast_point.y
